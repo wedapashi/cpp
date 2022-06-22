16af64fc150746804c37b0dcfde4a2820e55573b
WedaPashi10 <tandale.prasad@gmail.com>
2022-06-22 19:50:14 +0530

    Added contents for Operator overloading using friend function

--- 11_op_overloading_friend_function/README.md
+++ 11_op_overloading_friend_function/README.md
@@ -1,3 +1,29 @@
-# cpp
-These are C++ Programs scribbled during the knowledge-building of C++ programming. I understand and am aware of the fact that thse codes in any form as of today are not suitable/worthy of using in any of the ongoing developments.
+# Operator overloading
+
+Imagine, you are a mountaineer and at the same time frantic about statastics.
+You are Everesting -- meaning you are climbing a small hill nearby for multiple times until your total height gained is equal to 8848 m, which in fact is the height of Mt. Everest. The key is, you want to get to that goal in fixed time, so every minute counts!
+
+Your nephew is writing a code to add your lap-times. You specify the lap times in HH:MM format. Now, how incovenient it would be for him to add hours and minutes seperately. He would have to add hours first, then add minutes and process all chunks of 60 minutes into hours. But, he pulls off something like `T = T1+T2` where T1 and T2 contains two separate values -- hours and minutes, and still the addition goes through! 
+
+Here’s an example where the concept of addition makes sense, but the units that you are adding (a mixture of hours and minutes) don’t match a built-in type.
+
+Operator overloading extends the overloading concept to operators, letting you assign multiple meanings to C++ operators. In a pedantic sense, many operators in C and C++ are already overloaded, for example `*` -- It serves as a
+dereferencing operator as well as multiplication operator. 
+
+C++ lets you extend operator overloading to user-defeined types, allowing you, say to use `+` symbol to add two objects.
+Does C allow you to add two structs of the same data type? No, it doesn't. Can operator overloading allow implementation
+to add objects of the same class such that all data members of the one object are added with corresponding data member
+of the other object? Yes!
+
+The simple addition notation conceals the mechanics and emphasizes what is essential, and that is another goal of OOP.
+
+*Operator overloading* is an example of C++ Polymorphism.
+
+
+***
+### Program output:
+
+    Calling overloaded friend function..
+    Time: 5:40
+
 
--- 11_op_overloading_friend_function/main.cpp
+++ 11_op_overloading_friend_function/main.cpp
@@ -1,101 +1,40 @@
 #include <iostream>
 using namespace std;
 
-class Complex
+class Time
 {
 private:
-    uint32_t real;
-    uint32_t img;
+    uint32_t hours;
+    uint32_t minutes;
 public:
-    Complex();
-    Complex(uint32_t real, uint32_t img);
-    Complex(Complex &num);
-    void setReal(uint32_t real);
-    uint32_t getReal();
-    void setImg(uint32_t img);
-    uint32_t getImg();
-    Complex add(Complex C);
-    friend Complex operator+(Complex c1, Complex c2);
-    ~Complex();
+    Time() { 
+        hours = minutes = 0; 
+    }
+    Time(uint32_t hh, uint32_t min) {
+        hours = hh;
+        minutes = min;
+    }
+    friend Time operator+(Time t1, Time t2);
+    void print(void) {
+        cout << "Time: " << hours << ":" << minutes << endl;
+    }
+        
 };
-
-Complex::Complex()
+Time operator+(Time t1, Time t2)
 {
-    cout<<"Complex: In default constructor"<<endl;
-    setReal(1);
-    setImg(1);
-}
-Complex::Complex(uint32_t real, uint32_t img)
-{
-    cout<<"Complex: In parameterized constructor"<<endl;
-    setReal(real);
-    setImg(img);
-}
-Complex::Complex(Complex &num)
-{
-    cout<<"Complex: In copy constructor"<<endl;
-    setReal(num.real);
-    setImg(num.img);
-}
-void Complex::setReal(uint32_t real)
-{
-    this->real = real;
-}
-uint32_t Complex::getReal()
-{
-    return this->real;
-}
-void Complex::setImg(uint32_t img)
-{
-    this->img = img;
-}
-uint32_t Complex::getImg()
-{
-    return this->img;
-}
-Complex Complex::add(Complex C)
-{
-    Complex temp;
-    temp.real = this->real + C.real;
-    temp.img = this->img + C.img;
+    Time temp;
+    temp.minutes = t1.minutes + t2.minutes;
+    temp.hours = t1.hours + t2.hours + temp.minutes / 60;
+    temp.minutes %= 60;
     return temp;
-}
-Complex operator+(Complex c1, Complex c2)
-{
-    Complex temp;
-    //temp.setReal(c1.getReal() + c2.getReal());
-    //temp.setImg(c1.getImg() + c2.getImg());
-    temp.real=c1.real+c2.real;
-    temp.img=c1.img+c2.img;
-
-    return temp;
-}
-Complex::~Complex()
-{
-    cout<<"Complex: In destructor.."<<endl;
 }
 int main(void)
 {
-    Complex C1;
-    Complex C2(10, 50);
-    Complex C3(C2);
-    Complex C4;
-
-    cout<<"C1.real = "<<C1.getReal()<<endl;
-    cout<<"C1.img = "<<C1.getImg()<<endl;
-    cout<<"C2.real = "<<C2.getReal()<<endl;
-    cout<<"C2.img = "<<C2.getImg()<<endl;
-    cout<<"C3.real = "<<C3.getReal()<<endl;
-    cout<<"C3.img = "<<C3.getImg()<<endl;
-
-    C1 = C2.add(C3);
-    cout<<"C1=C2+C3: "<<C1.getReal()<<"+"<<C1.getImg()<<"i"<<endl;
-
+    Time T1(2, 56);
+    Time T2(2, 44);
+    Time T(0, 0);
     cout<<"Calling overloaded friend function.."<<endl;
-    C4 = C2+C3;
-    cout<<"C4=C2+C3: "<<C4.getReal()<<"+"<<C4.getImg()<<"i"<<endl;
-
-    cout<<endl;
-
+    T = T1+T2;
+    T.print();
     return 0;
 }
