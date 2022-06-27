81f8f02bb4925160ecdbea7a5447a35365108318
WedaPashi10 <tandale.prasad@gmail.com>
2022-06-27 12:27:40 +0530

    Updated the example code

--- 15_namespace/main.cpp
+++ 15_namespace/main.cpp
@@ -1,30 +1,23 @@
 #include<iostream>
-
 using namespace std;
-
 #define msg(x)  #x
 
-namespace Dev1
-{
-    void printVersion(void)
-    {
-        cout<<msg(V1.00)<<endl;
+namespace Dev1 {
+    void printVersion(void) {
+        cout << msg(V1.00) << endl;
     }
 };
 
-namespace Dev2
-{
-    void printVersion(void)
-    {
-        cout<<msg(V1.01)<<endl;
+namespace Dev2 {
+    void printVersion(void) {
+        cout << msg(V2.00) << endl;
     }
 };
 
-using namespace Dev1;
-int main(void)
-{
+int main(void) {    
+    using namespace Dev1;
     printVersion();
+    Dev1::printVersion();
     Dev2::printVersion();
-    
-    return 0;
+    return EXIT_SUCCESS;
 }
