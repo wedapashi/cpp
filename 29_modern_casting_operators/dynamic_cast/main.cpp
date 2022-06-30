1e6cb759145a40b0cb46831a13f9a2cd17001faa
WedaPashi10 <tandale.prasad@gmail.com>
2022-06-30 22:29:17 +0530

    Updated example code

--- 29_modern_casting_operators/dynamic_cast/main.cpp
+++ 29_modern_casting_operators/dynamic_cast/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <exception>
+#include <string>
 using namespace std;
 
 class Calibration {
@@ -14,18 +14,10 @@ class Calibration {
             gain = m;
             offset = c;
         }
-        double getGain(void) { 
-            return gain; 
-        }
-        double getOffset(void) { 
-            return offset; 
-        }
-        void setGain(double value) { 
-            gain = value; 
-        }
-        void setOffset(double value) { 
-            offset = value; 
-        }
+        double getGain(void) { return gain; }
+        double getOffset(void) { return offset; }
+        void setGain(double value) { gain = value; }
+        void setOffset(double value) { offset = value; }
         virtual void calibrate(void) {
             // Some code
         }
@@ -41,9 +33,7 @@ class sensorInformation:public Calibration {
         sensorInformation(string s_alias, double m, double c):Calibration(m, c) {
             setAlias(s_alias);
         }
-        string getAlias(void) { 
-            return alias; 
-        }
+        string getAlias(void) { return alias; }
         void setAlias(string s) { 
             alias.clear();
             alias = s;
@@ -58,16 +48,16 @@ int main (void) {
 
         pSensorInfo = dynamic_cast<sensorInformation*>(pCal_d);
         if (nullptr == pSensorInfo) {
-            cout << "Null pointer on first type-cast" << endl;
+            throw "Null pointer on first type-cast";
         }
         pSensorInfo = dynamic_cast<sensorInformation*>(pCal_b);
         if (nullptr == pSensorInfo) {
-            cout << "Null pointer on second type-cast" << endl;
+            throw "Null pointer on second type-cast";
         }
-    } catch (exception& e) {
-        cout << "Exception: " << e.what();
+
+    } catch (const char *msg) {
+        cout << "Exception: " << msg << endl;
     }
     
-    cout << endl;
     return EXIT_SUCCESS;
 }
