a11b8d1e7a51abc0ddfef3d1c16bf596968b1a23
WedaPashi <tandale.prasad@gmail.com>
2023-06-06 21:12:00 +0530

    Update main.cpp

--- 05_constructors/main.cpp
+++ 05_constructors/main.cpp
@@ -1,64 +1,39 @@
-#include <iostream>
-using namespace std;
+    #include <iostream>
+    using namespace std;
 
-class foo
-{
-private:
-    uint32_t bar;
-    uint32_t baz;
+    class foo
+    {
+    private:
+        uint32_t bar;
+    public:
+        // Parameterized constructor, can't return anything! This also acts as default argument constructor
+        // If default arguments are provided, the deault constructor is no longer required.
+        foo(uint32_t a=1)
+        {
+            cout<<"foo: In parameterized constructor"<<endl;
+            bar = a;
+        }
+        // Copy constructor
+        foo(foo &f)
+        {
+            cout<<"foo: In copy constructor"<<endl;
+            bar = f.bar;
+        }
+        uint32_t getBar(void) { return bar; }
+    };
 
-public:
-#if 0
-    // Default constructor, can't return anything!
-    foo()
-    {
-        cout<<"In foo: default constructor"<<endl;
-        bar = 1;
-        baz = 1;
+    void print(foo fTemp) {
+        std::cout << fTemp.getBar() << std::endl;
     }
-#endif
-    // Parameterized constructor, can't return anything! This also acts as default argument constructor
-    // If default arguments are provided, the deault constructor is no longer required.
-    foo(uint32_t a=1, uint32_t b=1)
-    {
-        cout<<"In foo: In parameterized constructor"<<endl;
-        setBar(bar);
-        setBaz(baz);
-    }
-    // Copy constructor
-    foo(foo &f)
-    {
-        cout<<"foo: In copy constructor"<<endl;
-        bar = f.bar;
-        baz = f.baz;
-    }
-    void setBar(uint32_t b) // Mutator
-    {
-        bar = b;
-    }
-    uint32_t getBar() // Facilitator
-    {
-        return bar;
-    }
-    void setBaz(uint32_t b) // Mutator
-    {
-        baz = b;
-    }
-    uint32_t getBaz() // Facilitator
-    {
-        return baz;
-    }
-    ~foo()
-    {
-        cout << "foo: In destructor" << endl;
-    }
-};
 
-int main(void)
-{
-    foo f1;  // Invokes default constructor
-    foo f2(10, 5); // Invokes paramterized constructor
-    foo f3(f2); // Invokes copy constructor
-
-    return 0;
-}
+    int main(void)
+    {
+        foo f1(10); // Invokes paramterized constructor
+        foo f2(f2); // Invokes copy constructor
+        foo f3 = 144U; 
+        // The following results in a compilation error as a compatible parameterized constructor is not found.
+        //foo f4 = "stupid";
+        print(f3);   // invokes copy constructor
+        print(250U); // invokes parameterized constructor
+        return 0;
+    }
