a5929e096a9c902f51d295ff75f5bcc4715affe2
WedaPashi <tandale.prasad@gmail.com>
2024-09-06 13:40:03 +0530

    Update main.cpp
    Coding style improvements

--- 05_constructors/main.cpp
+++ 05_constructors/main.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 using namespace std;
 
-class foo
-{
+class foo {
 private:
     uint32_t bar;
 public:
     // Parameterized constructor, can't return anything! This also acts as default argument constructor
     // If default arguments are provided, the deault constructor is no longer required.
-    foo(uint32_t a=1)
-    {
+    foo(uint32_t a=1) {
         cout<<"foo: In parameterized constructor"<<endl;
         bar = a;
     }
     // Copy constructor
-    foo(foo &f)
-    {
+    foo(foo &f) {
         cout<<"foo: In copy constructor"<<endl;
         bar = f.bar;
     }
@@ -26,8 +23,7 @@ void print(foo fTemp) {
     std::cout << fTemp.getBar() << std::endl;
 }
 
-int main(void)
-{
+int main(void) {
     foo f1(10); // Invokes paramterized constructor
     foo f2(f2); // Invokes copy constructor
     foo f3 = 144U; 
